372319145a5fee1fdc80dff55a3d5b61edbe7ca7
NatashaSenah <tashsenah@gmail.com>
2018-10-04 16:26:40 +0300

    add the modified instagrams

--- instagrams/models.py
+++ instagrams/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_image = models.ImageField(upload_to = 'image/')
+    image_name = models.CharField(max_length =30)
+    image_name = models.CharField(max_length =30)
+    profile = models.ForeignKey(Profile)
+    
