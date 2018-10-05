3b84bf8326ec6cf99dab6f0434927a46bf3a10fa
NatashaSenah <tashsenah@gmail.com>
2018-10-05 16:42:48 +0300

    add the modified instagrams

--- instagrams/views.py
+++ instagrams/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render,redirect
-from django.http  import HttpResponse
-
+from django.http  import HttpResponse,Http404
+from .models import Image
 # Create your views here.
 def welcome(request):
     return render(request,'welcome.html',{'name':'instagram'})
 
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_image = Image.search_by_image_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-instagram/search.html',{"message":message,"image": searched_image})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-instagram/search.html',{"message":message})
+
+
+def image(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-instagram/instagram.html", {"image":image})
