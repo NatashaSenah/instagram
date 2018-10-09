949a75c646702d4fa871c962fdb7c924122e1a4f
NatashaSenah <tashsenah@gmail.com>
2018-10-09 10:55:32 +0300

    deployment to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-from decouple import config
 import os
+import dj_database_url
+from decouple import config,Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
